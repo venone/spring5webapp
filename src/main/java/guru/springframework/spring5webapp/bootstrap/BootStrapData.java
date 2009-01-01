8a926366374b4d88816d6eb066fb9ade9cb27189
Pritesh <growupprit@gmail.com>
2009-01-01 15:14:08 +0530

    Adding Book and author in spring

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,71 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+//import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        /*
+        Publisher publisher = new Publisher();
+        publisher.setAddress("228 Rajaram Nagar");
+        publisher.setName("Sahyog House");
+        publisher.setCity("Dewas");
+        publisher.setState("M.P.");
+        publisher.setZip(455001L);
+        */
+
+        Author Shiva = new Author("Shiva","Shukla");
+        Book book_Shiva = new Book("Book of Radisys","123321");
+
+        // Add book to author
+        // Add author to book
+        Shiva.getBooks().add(book_Shiva);
+        book_Shiva.getAuthors().add(Shiva);
+        //book_Shiva.setPublisher(publisher);
+       // publisher.getBooks().add(book_Shiva);
+
+        authorRepository.save(Shiva);
+        bookRepository.save(book_Shiva);
+
+        System.out.println("Hi Pritesh !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("No of Books" + bookRepository.count());
+
+        Author Pritesh =new Author("Pritesh","Gethewale");
+        Book book_Pritesh = new Book("Book of truth","456987");
+        Pritesh.getBooks().add(book_Pritesh);
+        Pritesh.getBooks().add(book_Shiva);
+
+        book_Pritesh.getAuthors().add(Pritesh);
+        book_Shiva.getAuthors().add(Pritesh);
+
+        authorRepository.save(Pritesh);
+        bookRepository.save(book_Pritesh);
+
+        //publisher.getBooks().add(book_Pritesh);
+        //book_Pritesh.setPublisher(publisher);
+        //publisher.getBooks().add(book_Pritesh);
+
+        System.out.println("Today is 28th !!!!!");
+        System.out.println("No of Books"+bookRepository.count());
+
+        System.out.println("Books"+book_Shiva.getTitle());
+
+    }
+}
